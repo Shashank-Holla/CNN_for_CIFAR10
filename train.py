0360058ae142d8b3298e3e4b04cac262565e1fc4
Shashank-Holla <58994044+Shashank-Holla@users.noreply.github.com>
2020-03-10 23:38:31 +0530

    Updated Train

--- train.py
+++ train.py
@@ -34,7 +34,7 @@ def train(net, device, trainloader, optimizer, criterion, epoch):
         # print statistics
         running_loss += loss.item()
         epoch_train_loss += loss.item()
-        if i % 2000 == 1999:    # print every 2000 mini-batches
+        if i % 300 == 299:    # print every 300 mini-batches
             print('[%d, %5d] loss: %.3f' %
                   (epoch + 1, i + 1, running_loss / 2000))
             running_loss = 0.0
